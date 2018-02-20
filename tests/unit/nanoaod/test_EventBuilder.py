a43a228eafc7a0e02ffa31b28c4c5315530369fb
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-02-20 14:22:09 +0000

    Add unit tests for nanoaod

---
+++ tests/unit/nanoaod/test_EventBuilder.py
@@ -0,0 +1,69 @@
+import unittest
+import sys
+
+from alphatwirl.nanoaod import EventBuilderConfig
+
+##__________________________________________________________________||
+hasROOT = False
+try:
+    import ROOT
+    hasROOT = True
+except ImportError:
+    pass
+
+if hasROOT:
+    from alphatwirl.nanoaod.EventBuilder import EventBuilder
+    # depends on ROOT through roottree.BEventBuilder
+
+##__________________________________________________________________||
+class MockEvents(object):
+    def __init__(self, config):
+        self.config = config
+
+##__________________________________________________________________||
+class MockComponent(object):
+    pass
+
+##__________________________________________________________________||
+class MockBaseEventBuilder(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(self):
+        return MockEvents(self.config)
+
+##__________________________________________________________________||
+class MockBaseConfig(object):
+    pass
+
+##__________________________________________________________________||
+@unittest.skipUnless(hasROOT, "has no ROOT")
+class TestEventBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.module = sys.modules['alphatwirl.nanoaod.EventBuilder']
+        self.org_BaseEventBuilder = self.module.BaseEventBuilder
+        self.module.BaseEventBuilder = MockBaseEventBuilder
+
+    def tearDown(self):
+        self.module.BaseEventBuilder = self.org_BaseEventBuilder
+
+    def test_build(self):
+
+        base_config = MockBaseConfig
+        component = MockComponent()
+
+        config = EventBuilderConfig(
+            base = base_config,
+            component = component,
+        )
+
+        obj = EventBuilder(config)
+
+        events = obj()
+
+        self.assertIs(base_config, events.config)
+        self.assertIs(component, events.component)
+
+##__________________________________________________________________||
