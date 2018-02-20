e5e3acde1449efc770f399877db00a5bc73cb3be
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-02-20 13:01:58 +0000

    Add some additional comments

--- alphatwirl/nanoaod/NanoAODResult.py
+++ alphatwirl/nanoaod/NanoAODResult.py
@@ -1,5 +1,3 @@
-# Tai Sakuma <tai.sakuma@gmail.com>
-
 ##__________________________________________________________________||
 import os
 import re
@@ -12,41 +10,34 @@ class NanoAODResult(object):
     """A NanoAOD result
 
     Args:
-        path (str): the path to the Heppy result
-        component_names (list, optional): the list of the names of the components to read. If not given, all components except the ones listed in `excludeList` will be read.
-        exclude_list (list, optional): a list of the names of the directory in the nanoAOD result directory which are to be excluded to be considered as component.
+        component_df (pd.DataFrame): the dataframe of components on each row
+        the list of the names of the components to read. If not given, all components except the ones listed in `excludeList` will be read.
     """
 
-    def __init__(self,
-                 component_df=None,
-                 exclude_list=[],
-    ):
-        component_df["name"] = component_df[["dataset","era"]].apply("_".join, axis=1)
+    def __init__(self, component_df):
+        component_df["name"] = component_df[["dataset","era"]].apply("_".join, axis=1) # Name for the progress bar
         self.components_df = component_df
-
-    #def __getattr__(self, name):
-    #    if name not in self.components_df:
-    #        raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
-    #    return os.path.join(self.path, name)
 
     def components(self):
         comps = []
         for row in self.components_df.itertuples():
-            comp_dict = {}
-            comp_dict["name"] = row.name
-            comp_dict["eventtype"] = row.eventtype
-            comp_dict["dataset"] = row.dataset
-            comp_dict["era"] = row.era
-            comp_dict["nevents"] = row.nevents
-            comp_dict["nfiles"] = row.nfiles
-            comp_dict["files"] = row.files
+            comp_dict = dict(
+                name      = row.name
+                eventtype = row.eventtype
+                dataset   = row.dataset
+                era       = row.era
+                nevents   = row.nevents
+                nfiles    = row.nfiles
+                files     = row.files
+            )
 
-            # MC stuff
+            # MC related information. Just XS for now
             try:
                 comp_dict["cross_section"] = row.cross_section
             except AttributeError:
                 comp_dict["cross_section"] = np.nan
 
+            # Add the component information as a named tuple to a list
             comps.append(Component(**comp_dict))
         return comps
 
