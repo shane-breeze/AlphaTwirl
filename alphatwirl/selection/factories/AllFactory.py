c3651bbb5074ff27034e21f983e6a7c8abc7383d
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-02-20 15:47:07 +0000

    Revert "Allow additional parameters to be sent to the All, Any, Not classes"
    This reverts commit 422abfdcd3a15ba8b9dbd8c7b29824241d3832a9.

--- alphatwirl/selection/factories/AllFactory.py
+++ alphatwirl/selection/factories/AllFactory.py
@@ -5,7 +5,6 @@ from .FactoryDispatcher import FactoryDispatcher
 def AllFactory(path_cfg_list, name = None,  **kargs):
 
     ret = kargs['AllClass'](name = name)
-    ret.set_args(kargs)
 
     for path_cfg in path_cfg_list:
         ret.add(FactoryDispatcher(path_cfg, **kargs))
--- alphatwirl/selection/factories/AnyFactory.py
+++ alphatwirl/selection/factories/AnyFactory.py
@@ -5,7 +5,6 @@ from .FactoryDispatcher import FactoryDispatcher
 def AnyFactory(path_cfg_list, name = None,  **kargs):
 
     ret = kargs['AnyClass'](name = name)
-    ret.set_args(kargs)
 
     for path_cfg in path_cfg_list:
         ret.add(FactoryDispatcher(path_cfg, **kargs))
